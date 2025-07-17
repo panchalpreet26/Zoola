8545563d963e559612fd24a5f13c52c6b3e6543b
panchalpreet26 <panchalpreet26@gmail.com>
2025-07-17 17:42:10 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Zulas",{
+require("dotenv").config();
+mongoose.connect("process.env.MONGO_URL",{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{console.log("MongoDB Connected Successfully")}).catch((e)=>{console.log(e);});
