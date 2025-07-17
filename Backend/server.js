a9a8bd7c25cad2e9689b98d17d97904ba8a97a37
panchalpreet26 <panchalpreet26@gmail.com>
2025-07-17 16:31:41 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,11 @@ require("./db");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://zoola.onrender.com", //
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
